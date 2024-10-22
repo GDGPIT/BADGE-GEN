faba5dad3d2c6eddd139e145e615fe578298a01c
GDGPIT <nara16761@gmail.com>
2024-10-22 08:41:15 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# BADGE-GEN
+# GDSC Digital Badge Generator
+
+## Introduction
+A Digital Badge Generator for Google Developer Groups on Campus Panimalar Institute of Technology members.
---
+++ main.js
@@ -0,0 +1,64 @@
+var loadFile = (event) => {
+    let image = document.getElementById('photo');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+var loadName = (event) => {
+    let name = document.getElementById("name");
+    name.innerHTML = event.target.value;
+};
+
+var loadPosition = (event) => {
+    let position = document.getElementById("position");
+    position.innerHTML = event.target.value;
+};
+
+const downloadImg = () => {
+    var card = document.getElementById("card");
+    var nameVal = document.getElementById("name").innerHTML;
+    var positionVal = document.getElementById("position").innerHTML;
+
+    html2canvas(card, {
+        scale: 1.1,
+        backgroundColor: "rgba(1,1,1,0)"
+    }).then(function(canvas) {
+        var imgLink = document.createElement("a");
+        document.body.appendChild(imgLink);
+        imgLink.download = `${nameVal}-${positionVal}.png`; // Include both name and position in the file name
+        imgLink.href = canvas.toDataURL("image/png");
+        imgLink.target = '_blank';
+        imgLink.click();
+    });
+};
+
+function onNavButtonClick() {
+    let navBar = document.getElementById("navbarSupportedContent");
+    let button = document.getElementById('navbar-custom-button');
+    let arial = button.getAttribute("aria-expanded");
+    navBar.classList.replace('collapse', 'collapsing');
+    if (navBar.classList.contains('show')) {
+        button.classList.add('collapsed');
+        arial = false;
+        navBar.classList.remove('show');
+    } else {
+        button.classList.remove('collapsed');
+        arial = true;
+        navBar.classList.add('show');
+    }
+    button.setAttribute("aria-expanded", arial);
+    setTimeout(() => {
+        navBar.classList.replace('collapsing', 'collapse');
+    }, 95);
+}
+
+function onDropDownActivate() {
+    let dropdown = document.getElementById("navbarLightDropdownMenuLink");
+    let ul = document.getElementById('ulLightDropDown');
+    if (dropdown.classList.contains('show')) {
+        ul.classList.remove('show');
+        dropdown.classList.remove('show');
+    } else {
+        ul.classList.add('show');
+        dropdown.classList.add('show');
+    }
+}
